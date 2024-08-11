ff37f50e78fc76e9e50f036727ca3659b60be571
Mayza Ferreira <mayza_ferr@hotmail.com>
2024-08-11 18:22:10 -0300

    feat: add Header component and banner

---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
+import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl">
+          Olá, <span className="font-bold">Felipe!</span>
+        </h2>
+        <p>Sexta, 2 de Fevereiro</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Buscar" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Home
