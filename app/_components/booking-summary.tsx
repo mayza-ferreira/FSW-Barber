d2e4ae1687c5fc6ebbc1a4c122b0b80df80b7ac0
Mayza Ferreira <mayza_ferr@hotmail.com>
2024-10-19 22:37:14 -0300

    feat: ensure booking deatails can be viewed

---
+++ app/_components/booking-summary.tsx
@@ -0,0 +1,10 @@
+// import { ptBR } from "date-fns/locale"
+// import { format } from "date-fns"
+// import { Card, CardContent } from "./ui/card"
+
+//TODO: Create sukmary component
+const BookingSummary = () => {
+  return <></>
+}
+
+export default BookingSummary
